98531ccd6cdb79cad242bf19e16c398d838b87e7
Артур Мухайлов <arthurcloud@yandex.ru>
2020-03-02 23:28:31 +0500

    Добавьте файлы проекта.

---
+++ LINQ BEGIN 34/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        // Вариант 2 Beginer
+        // Задача 34
+        static void Main(string[] args)
+        {
+            List<int> list2 = new List<int> { 2, 13, 33, 53, 9, 56, 58, 7, 9, 54, 87, 41, 86 };
+            var result3 = list2.Where(x => x % 2 == 0).Select(x => x.ToString()).OrderBy(x => x);
+            foreach (string i in result3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
